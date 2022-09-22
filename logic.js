6e01bcd6da4eff5e6854837fec54926519b65e4d
Jonathan Killen <Productionmax@hotmail.com>
2022-09-22 12:25:35 +0800

    Made form of adding books with html

--- logic.js
+++ logic.js
@@ -1,11 +1,25 @@
+const theHobbit = new book('The Hobbit', 'J.R.R. Tolkien','295 pages', 'not read yet');
+const nineteenEightyFour = new book('1984','George Orwell','328','Have read');
+const theColorPurple = new book ('The Color Purple','Alice Walker','200','Have read');
+let myLibrary = [theHobbit,nineteenEightyFour,theColorPurple];
+
 function book(title,author,pages,status){
     this.title = title;
-    this.author=author;
+    this.author = author;
     this.pages = pages;
     this.status = status;
     this.info = function(){
         return(`${title} ${author}, ${pages}, ${status}`)
     }
 }
-const theHobbit = new book('The Hobbit', 'J.R.R. Tolkien','295 pages', 'not read yet');
-console.log(theHobbit.info());
+
+
+// function addBookToLibrary(){
+    
+// }
+function displayBook(){
+    myLibrary.forEach((books)=>{
+        return (books);
+    });
+}
+console.log(displayBook());
