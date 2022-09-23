5ccb86f140870612efd5cd118f79d1d60135c113
Jonathan Killen <Productionmax@hotmail.com>
2022-09-23 13:53:47 +0800

    Made form with HTML to add books

--- logic.js
+++ logic.js
@@ -3,6 +3,13 @@ const nineteenEightyFour = new book('1984','George Orwell','328','Have read');
 const theColorPurple = new book ('The Color Purple','Alice Walker','200','Have read');
 let myLibrary = [theHobbit,nineteenEightyFour,theColorPurple];
 
+const submit = document.querySelector('.addBook');
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const pages = document.getElementById('pages');
+const status = document.getElementById('status');
+submit.addEventListener('click',addBookToLibrary);
+//COnstructor for the books
 function book(title,author,pages,status){
     this.title = title;
     this.author = author;
@@ -13,13 +20,13 @@ function book(title,author,pages,status){
     }
 }
 
+//Adding book to library
 
-// function addBookToLibrary(){
-    
-// }
-function displayBook(){
-    myLibrary.forEach((books)=>{
-        return (books);
-    });
+function addBookToLibrary(){
+    let newBook = new book(title.value , author.value , pages.value, status.value);
+    return newBook.info;
 }
-console.log(displayBook());
+console.log(addBookToLibrary());
+//take user input
+//Store to the array
+//
