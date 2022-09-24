baaad165b74c8f6e5adcd6f29bacf0da268f9a34
Jonathan Killen <Productionmax@hotmail.com>
2022-09-24 22:28:01 +0800

    Cards output the books in the array

--- logic.js
+++ logic.js
@@ -19,7 +19,38 @@ function book(title,author,pages,status){
 const theHobbit = new book('The Hobbit', 'J.R.R. Tolkien','295 pages', 'not read yet');
 const nineteenEightyFour = new book('1984','George Orwell','328','Have read');
 const theColorPurple = new book ('The Color Purple','Alice Walker','200','Have read');
+//Displaying Books
+function displayBooks(){
+    myLibrary.forEach(function(i){
+        //create new divs for each piece of book info
+        const title = document.createElement('div');
+        const author = document.createElement('div');
+        const pages = document.createElement('div');
+        const status = document.createElement('div');
+        //Setting the div innertext to each book information
+        title.innerText = i.title;
+        author.innerText = i.author;
+        pages.innerText = i.pages;
+        status.innerText = i.status
+        
+        //Creating a container within the card
+        const bookContainer = document.createElement('div');
+        bookContainer.classList.add('book-container');
 
+        //Appendchild of the created divs to the newly created bookcontainer div
+        bookContainer.appendChild(title);
+        bookContainer.appendChild(author);
+        bookContainer.appendChild(pages);
+        bookContainer.appendChild(status);
+
+        document.querySelector('.cards').appendChild(bookContainer);
+    })
+}
+displayBooks();
+//1. Loop through the array
+//2. array has several books 
+//3. Output the books in certain index position to otusdie
+//4. Make new divs to put inside the div
 //Adding book to library
 function addBookToLibrary(){
     const title = document.getElementById('title').value;
