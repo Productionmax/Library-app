70c8292d65006ea84669e1732a557b8c65e45f98
Jonathan Killen <Productionmax@hotmail.com>
2022-09-24 20:45:11 +0800

    Added sample book objects to the library array

--- logic.js
+++ logic.js
@@ -1,13 +1,8 @@
-const theHobbit = new book('The Hobbit', 'J.R.R. Tolkien','295 pages', 'not read yet');
-const nineteenEightyFour = new book('1984','George Orwell','328','Have read');
-const theColorPurple = new book ('The Color Purple','Alice Walker','200','Have read');
-let myLibrary = [theHobbit,nineteenEightyFour,theColorPurple];
 
+let myLibrary = [];
+const form = document.querySelector("#bookform");
 const submit = document.querySelector('.addBook');
-const title = document.getElementById('title');
-const author = document.getElementById('author');
-const pages = document.getElementById('pages');
-const status = document.getElementById('status');
+//Submit enable function to addBookToLibrary
 submit.addEventListener('click',addBookToLibrary);
 //COnstructor for the books
 function book(title,author,pages,status){
@@ -15,18 +10,27 @@ function book(title,author,pages,status){
     this.author = author;
     this.pages = pages;
     this.status = status;
-    this.info = function(){
-        return(`${title} ${author}, ${pages}, ${status}`)
+    this.info = function (){
+        return (`Book Title`+ title + `by` + author + `Page Count:` + pages + `Read status:` + status)
     }
+    myLibrary.push(this);
 }
+//Sample books
+const theHobbit = new book('The Hobbit', 'J.R.R. Tolkien','295 pages', 'not read yet');
+const nineteenEightyFour = new book('1984','George Orwell','328','Have read');
+const theColorPurple = new book ('The Color Purple','Alice Walker','200','Have read');
 
 //Adding book to library
-
 function addBookToLibrary(){
-    let newBook = new book(title.value , author.value , pages.value, status.value);
-    return newBook.info;
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const status = document.getElementById('status').value;
+    let newBook = new book(title, author , pages, status);
+    myLibrary.push(newBook.info);
+    console.log(myLibrary);
 }
-console.log(addBookToLibrary());
+
 //take user input
 //Store to the array
 //
